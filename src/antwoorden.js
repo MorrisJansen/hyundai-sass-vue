76fe3c1144cb9df61de0b412f014eb007a0792cd
MorrisJansen <morris8jansen@hotmail.com>
2024-06-13 16:19:32 +0200

    gh-pages commit

--- src/antwoorden.js
+++ src/antwoorden.js
@@ -1,14 +1,9 @@
-// antwoorden.js
-
-// Initial state
 let antwoorden = [];
 
-// Function to add a new answer to the state
 export function addAntwoord(answer) {
   antwoorden.push(answer);
 }
 
-// Function to get all answers
 export function getAntwoorden() {
   return antwoorden;
 }
