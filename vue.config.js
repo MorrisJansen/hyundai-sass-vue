4a7587b30e8ef6f631e9c297031c158c952d68b0
MorrisJansen <morris8jansen@hotmail.com>
2024-06-14 10:09:57 +0200

    index.html route aangepast voor main.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/hyundai-sass-vue/' : '/'
-}
-
-
-module.exports = {
-  publicPath : './'
-}
-
-module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/hyundai-sass-vue/' : '/',
   configureWebpack: {
     optimization: {
       minimize: false
